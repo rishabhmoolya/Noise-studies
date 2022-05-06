4ca2308c1a41725de6632d1563b270b8af1525bf
Rishabh Moolya <52218346+rishabhmoolya@users.noreply.github.com>
2022-05-06 17:30:42 +0200

    Update Trial.py

--- Trial.py
+++ Trial.py
@@ -24,7 +24,7 @@ fm = pd.DataFrame(fm)
 fm.to_csv('Noisy_m595(13th April).csv')
 
 Dfinal = np.array([])
-o_stuck = np.array([])
+O_stuck = np.array([])
 noisy = []
 stuck = []
 same = []
@@ -152,9 +152,10 @@ for j in range(26, 19, -6):
             # Stuck pixels:
             Data2 = np.array(data_s1[0])
             d2 = 192*136- Enabled_s2[0].size
-            print(f"Masked pixels after stuck pixels occupancy scan:{d2 - d1}")
+            o_stuck = (d2-d1)
+            print(f"Masked pixels after stuck pixels occupancy scan:{o_stuck}")
+            O_stuck = np.append(O_stuck,o_stuck)
             
-            o_stuck = np.append(o_stuck,(d2-d1))
             
             # 2nd Noise occupancy:
             Data3 = np.array(datan2[0])
@@ -196,10 +197,13 @@ for j in range(26, 19, -6):
 # =============================================================================
 
             pn = percentage(Dfinal)
-            ps = percentage(o_stuck)
+            ps = percentage(O_stuck)
             pv_np600 = percentage(v_np600)
             pv_np700 = percentage(v_np700)
             pv_np800 = percentage(v_np800)
+            pv_s600 = percentage(v_s600)
+            pv_s700 = percentage(v_s700)
+            pv_s800 = percentage(v_s800)
             # Differentiating between types of pixels(with rows 0 and 191):
             sum = Maskn2 + M2 + Mask_2
             c1 = []
@@ -349,152 +353,183 @@ print(f"The position of Same masked pixels without row 0 and 191:{list(c_3)}"+ "
 # plt.show()
 # =============================================================================
 
-# =============================================================================
-# plt.figure(7)
-# plt.ylabel('Rows in LIN FE(0 to 191',fontname="Times New Roman", fontweight="bold")
-# plt.title('Output of BDAQ scans',fontname="Times New Roman", size = 14, fontweight="bold")
-# plt.xlabel('Columns in LIN FE(128 to 264)',fontname="Times New Roman", fontweight="bold")
-# s = plt.imshow(sum[0:192,128:264]) 
-# bar = plt.colorbar(s,ticks =[3.0,2.0,1.0,0.0])
-# bar.set_label('Noisy pixels', rotation=270,fontname="Times New Roman", fontweight="bold")
-# bar.ax.set_yticklabels(['3.0(good pixel)','2.0(same masked pixel)','1.0(stuck pixel)','0.0(noisy/dead pixel)'])
-# plt.show()
-# 
-# # Noisy pixels Vs Voltage at constant Temperature:
-# plt.figure(8)
-# plt.ylabel('Noisy Pixels[%]',fontname="Times New Roman", fontweight="bold")
-# plt.title('Noisy pixels vs Voltage(595, bitten, $0.862$e16 $n_{eq} . cm^{-2}$)',fontname="Times New Roman", fontweight="bold")
-# plt.xlabel('Voltage(V)',fontname="Times New Roman", fontweight="bold")
-# plt.axis([None, None, 0, 5])
-# plt.yticks(np.arange(0,5.5,0.5))
-# plt.rcParams["figure.figsize"] = [7.50,3.50]
-# plt.rcParams["figure.autolayout"] = True
-# plt.grid(color = 'black', linestyle = '--', linewidth = 0.5)
-# plt.axhline(y = 1, xmin=0, xmax=1, color='k', linestyle='--', linewidth=2)
-# plt.annotate('[261 pixels]',ha = 'center', va = 'bottom', xytext = (0.2, 1.1),xy = (0.2, 1),arrowprops = {'facecolor' : 'black'})
-# 
-# line1 = plt.plot(V[0], pn[9], 'ro', lw=2, label= 'T = -20℃(noisy)')
-# line2 = plt.plot(V[0], pn[10], 'ro', lw=1.5)
-# line3 = plt.plot(V[0], pn[11], 'ro', lw=1.5)
-# 
-# line01 = plt.plot(V[0], ps[9], 'bo', lw=2, label= 'T = -20℃(stuck)')
-# line02 = plt.plot(V[0], ps[10], 'bo', lw=1.5)
-# line03 = plt.plot(V[0], ps[11], 'bo', lw=1.5)
-# 
-# line4 = plt.plot(V[1], pn[12], 'ro', lw=2)
-# line5 = plt.plot(V[1], pn[13], 'ro', lw=1.5)
-# line6 = plt.plot(V[1], pn[14], 'ro', lw=1.5)
-# 
-# line04 = plt.plot(V[1], ps[12], 'bo', lw=2)
-# line05 = plt.plot(V[1], ps[13], 'bo', lw=1.5)
-# line06 = plt.plot(V[1], ps[14], 'bo', lw=1.5)
-# 
-# line7 = plt.plot(V[2], pn[15], 'ro', lw=2)
-# line8 = plt.plot(V[2], pn[16], 'ro', lw=1.5)
-# line9 = plt.plot(V[2], pn[17], 'ro', lw=1.5)
-# 
-# line07 = plt.plot(V[2], ps[15], 'bo', lw=2)
-# line08 = plt.plot(V[2], ps[16], 'bo', lw=1.5)
-# line09 = plt.plot(V[2], ps[17], 'bo', lw=1.5)
-# 
-# line10 = plt.plot(V[3], pn[18], 'ro', lw=2)
-# line11= plt.plot(V[3], pn[19], 'ro', lw=1.5)
-# line12= plt.plot(V[3], pn[20], 'ro', lw=1.5)
-# 
-# line010 = plt.plot(V[3], ps[18], 'bo', lw=2)
-# line011= plt.plot(V[3], ps[19], 'bo', lw=1.5)
-# line012= plt.plot(V[3], ps[20], 'bo', lw=1.5)
-# 
-# line13 = plt.plot(V[4], pn[21], 'ro', lw=2)
-# line14 = plt.plot(V[4], pn[22], 'ro', lw=1.5)
-# line15= plt.plot(V[4], pn[23], 'ro', lw=1.5)
-# 
-# line013 = plt.plot(V[4], ps[21], 'bo', lw=2)
-# line014 = plt.plot(V[4], ps[22], 'bo', lw=1.5)
-# line015= plt.plot(V[4], ps[23], 'bo', lw=1.5)
-# 
-# line16 = plt.plot(V[5], pn[24], 'ro', lw=2)
-# line17 = plt.plot(V[5], pn[25], 'ro', lw=1.5)
-# line18 = plt.plot(V[5], pn[26], 'ro', lw=1.5)
-# 
-# line016 = plt.plot(V[5], ps[24], 'bo', lw=2)
-# line017 = plt.plot(V[5], ps[25], 'bo', lw=1.5)
-# line018 = plt.plot(V[5], ps[26], 'bo', lw=1.5)
-# 
-# line19 = plt.plot(V[3], pn[0], 'ks', lw=2, label= 'T = -26℃(noisy)')
-# line20 = plt.plot(V[3], pn[1], 'ks', lw=1.5)
-# line21 = plt.plot(V[3], pn[2], 'ks', lw=1.5)
-# 
-# line019 = plt.plot(V[3], ps[0], 'gs', lw=2, label= 'T = -26℃(stuck)')
-# line020 = plt.plot(V[3], ps[1], 'gs', lw=1.5)
-# line021 = plt.plot(V[3], ps[2], 'gs', lw=1.5)
-# 
-# line22 = plt.plot(V[4], pn[3], 'ks', lw=2)
-# line23 = plt.plot(V[4], pn[4], 'ks', lw=1.5)
-# line24 = plt.plot(V[4], pn[5], 'ks', lw=1.5)
-# 
-# line022 = plt.plot(V[4], ps[3], 'gs', lw=2)
-# line023 = plt.plot(V[4], ps[4], 'gs', lw=1.5)
-# line024 = plt.plot(V[4], ps[5], 'gs', lw=1.5)
-# 
-# line25 = plt.plot(V[5], pn[6], 'ks', lw=2)
-# line26 = plt.plot(V[5], pn[7], 'ks', lw=1.5)
-# line27 = plt.plot(V[5], pn[8], 'ks', lw=1.5)
-# 
-# line025 = plt.plot(V[5], ps[6], 'gs', lw=2)
-# line026 = plt.plot(V[5], ps[7], 'gs', lw=1.5)
-# line027 = plt.plot(V[5], ps[8], 'gs', lw=1.5)
-# 
-# plt.legend()
-# plt.show()
-# =============================================================================
+plt.figure(7)
+plt.ylabel('Rows in LIN FE(0 to 191',fontname="Times New Roman", fontweight="bold")
+plt.title('Output of BDAQ scans',fontname="Times New Roman", size = 14, fontweight="bold")
+plt.xlabel('Columns in LIN FE(128 to 264)',fontname="Times New Roman", fontweight="bold")
+s = plt.imshow(sum[0:192,128:264]) 
+bar = plt.colorbar(s,ticks =[3.0,2.0,1.0,0.0])
+bar.set_label('Noisy pixels', rotation=270,fontname="Times New Roman", fontweight="bold")
+bar.ax.set_yticklabels(['3.0(good pixel)','2.0(same masked pixel)','1.0(stuck pixel)','0.0(noisy/dead pixel)'])
+plt.show()
+
+# Noisy pixels Vs Voltage at constant Temperature:
+plt.figure(8)
+plt.ylabel('Noisy Pixels[%]',fontname="Times New Roman", fontweight="bold")
+plt.title('Noisy pixels vs Voltage(595, bitten, $0.862$e16 $n_{eq} . cm^{-2}$)',fontname="Times New Roman", fontweight="bold")
+plt.xlabel('Voltage(V)',fontname="Times New Roman", fontweight="bold")
+plt.axis([None, None, 0, 3])
+plt.yticks(np.arange(0,3.5,0.5))
+plt.rcParams["figure.figsize"] = [7.50,3.50]
+plt.rcParams["figure.autolayout"] = True
+plt.grid(color = 'black', linestyle = '--', linewidth = 0.5)
+plt.axhline(y = 1, xmin=0, xmax=1, color='k', linestyle='--', linewidth=2)
+plt.annotate('[261 pixels]',ha = 'center', va = 'bottom', xytext = (0.2, 1.1),xy = (0.2, 1),arrowprops = {'facecolor' : 'black'})
+
+line1 = plt.plot(V[0], pn[9], 'ro', lw=2, label= 'T = -20℃(noisy)')
+line2 = plt.plot(V[0], pn[10], 'ro', lw=1.5)
+line3 = plt.plot(V[0], pn[11], 'ro', lw=1.5)
+
+line01 = plt.plot(V[0], ps[9], 'bo', lw=2, label= 'T = -20℃(stuck)')
+line02 = plt.plot(V[0], ps[10], 'bo', lw=1.5)
+line03 = plt.plot(V[0], ps[11], 'bo', lw=1.5)
+
+line4 = plt.plot(V[1], pn[12], 'ro', lw=2)
+line5 = plt.plot(V[1], pn[13], 'ro', lw=1.5)
+line6 = plt.plot(V[1], pn[14], 'ro', lw=1.5)
+
+line04 = plt.plot(V[1], ps[12], 'bo', lw=2)
+line05 = plt.plot(V[1], ps[13], 'bo', lw=1.5)
+line06 = plt.plot(V[1], ps[14], 'bo', lw=1.5)
+
+line7 = plt.plot(V[2], pn[15], 'ro', lw=2)
+line8 = plt.plot(V[2], pn[16], 'ro', lw=1.5)
+line9 = plt.plot(V[2], pn[17], 'ro', lw=1.5)
+
+line07 = plt.plot(V[2], ps[15], 'bo', lw=2)
+line08 = plt.plot(V[2], ps[16], 'bo', lw=1.5)
+line09 = plt.plot(V[2], ps[17], 'bo', lw=1.5)
+
+line10 = plt.plot(V[3], pn[18], 'ro', lw=2)
+line11= plt.plot(V[3], pn[19], 'ro', lw=1.5)
+line12= plt.plot(V[3], pn[20], 'ro', lw=1.5)
+
+line010 = plt.plot(V[3], ps[18], 'bo', lw=2)
+line011= plt.plot(V[3], ps[19], 'bo', lw=1.5)
+line012= plt.plot(V[3], ps[20], 'bo', lw=1.5)
+
+line13 = plt.plot(V[4], pn[21], 'ro', lw=2)
+line14 = plt.plot(V[4], pn[22], 'ro', lw=1.5)
+line15= plt.plot(V[4], pn[23], 'ro', lw=1.5)
+
+line013 = plt.plot(V[4], ps[21], 'bo', lw=2)
+line014 = plt.plot(V[4], ps[22], 'bo', lw=1.5)
+line015= plt.plot(V[4], ps[23], 'bo', lw=1.5)
+
+line16 = plt.plot(V[5], pn[24], 'ro', lw=2)
+line17 = plt.plot(V[5], pn[25], 'ro', lw=1.5)
+line18 = plt.plot(V[5], pn[26], 'ro', lw=1.5)
+
+line016 = plt.plot(V[5], ps[24], 'bo', lw=2)
+line017 = plt.plot(V[5], ps[25], 'bo', lw=1.5)
+line018 = plt.plot(V[5], ps[26], 'bo', lw=1.5)
+
+line19 = plt.plot(V[3], pn[0], 'ks', lw=2, label= 'T = -26℃(noisy)')
+line20 = plt.plot(V[3], pn[1], 'ks', lw=1.5)
+line21 = plt.plot(V[3], pn[2], 'ks', lw=1.5)
+
+line019 = plt.plot(V[3], ps[0], 'gs', lw=2, label= 'T = -26℃(stuck)')
+line020 = plt.plot(V[3], ps[1], 'gs', lw=1.5)
+line021 = plt.plot(V[3], ps[2], 'gs', lw=1.5)
+
+line22 = plt.plot(V[4], pn[3], 'ks', lw=2)
+line23 = plt.plot(V[4], pn[4], 'ks', lw=1.5)
+line24 = plt.plot(V[4], pn[5], 'ks', lw=1.5)
+
+line022 = plt.plot(V[4], ps[3], 'gs', lw=2)
+line023 = plt.plot(V[4], ps[4], 'gs', lw=1.5)
+line024 = plt.plot(V[4], ps[5], 'gs', lw=1.5)
+
+line25 = plt.plot(V[5], pn[6], 'ks', lw=2)
+line26 = plt.plot(V[5], pn[7], 'ks', lw=1.5)
+line27 = plt.plot(V[5], pn[8], 'ks', lw=1.5)
+
+line025 = plt.plot(V[5], ps[6], 'gs', lw=2)
+line026 = plt.plot(V[5], ps[7], 'gs', lw=1.5)
+line027 = plt.plot(V[5], ps[8], 'gs', lw=1.5)
+
+plt.legend()
+plt.show()
 
 # Noisy pixels Vs Temperature at constant Voltage:
 plt.figure(9)
 tp = [-26,-20]
 plt.ylabel('No. of Noisy Pixels',fontname="Times New Roman", fontweight="bold")
-plt.title('Noisy pixels vs Temperature[Without Stuck](595, bitten, $0.862$e16 $n_{eq} . cm^{-2}$)',fontname="Times New Roman", fontweight="bold")
+plt.title('Noisy pixels vs Temperature[Only Noisy](595, bitten, $0.862$e16 $n_{eq} . cm^{-2}$)',fontname="Times New Roman", fontweight="bold")
 plt.xlabel('Temperature(℃)',fontname="Times New Roman", fontweight="bold")
-plt.axis([None, None, 0, max(v_np800)+50])
-plt.yticks(np.arange(0,max(v_np800)+10,50))
+plt.axis([None, None, 0, 2.5])
+plt.yticks(np.arange(0,2.5,0.5))
 plt.rcParams["figure.figsize"] = [7.50,3.50]
 plt.rcParams["figure.autolayout"] = True
 #plt.xticks(ticks = tickvalues ,labels = labellist, rotation = 'vertical')
-line1 = plt.plot(tp[0], v_np600[0], 'b--o', lw=1.5, label= '600 V @ T = -26°C')
-line2 = plt.plot(tp[0], v_np600[1], 'b--o', lw=1.5)
-line3 = plt.plot(tp[0], v_np600[2], 'b--o', lw=1.5)
+line1 = plt.plot(tp[0], pv_np600[0], 'r--o', lw=1.5, label= '600 V @ T = -26°C')
+line2 = plt.plot(tp[0], pv_np600[1], 'r--o', lw=1.5)
+line3 = plt.plot(tp[0], pv_np600[2], 'r--o', lw=1.5)
 
-line01 = plt.plot(tp[0], v_s600[0], 'b--*', lw=1.5, label= '600 V @ T = -26°C')
-line02 = plt.plot(tp[0], v_s600[1], 'b--*', lw=1.5)
-line03 = plt.plot(tp[0], v_s600[2], 'b--*', lw=1.5)
+line4 = plt.plot(tp[1], pv_np600[3], 'r--v', lw=1.5, label= '600 V @ T = -20°C')
+line5 = plt.plot(tp[1], pv_np600[4], 'r--v', lw=1.5)
+line6 = plt.plot(tp[1], pv_np600[5], 'r--v', lw=1.5)
 
-line4 = plt.plot(tp[1], v_np600[3], 'r--o', lw=1.5, label= '600 V @ T = -20°C')
-line5 = plt.plot(tp[1], v_np600[4], 'r--o', lw=1.5)
-line6 = plt.plot(tp[1], v_np600[5], 'r--o', lw=1.5)
+line7 = plt.plot(tp[0], pv_np700[0], 'b--o', lw=1.5, label= '700 V @ T = -26°C')
+line8 = plt.plot(tp[0], pv_np700[1], 'b--o', lw=1.5)
+line9 = plt.plot(tp[0], pv_np700[2], 'b--o', lw=1.5)
 
-line04 = plt.plot(tp[1], v_s600[3], 'r--*', lw=1.5, label= '600 V @ T = -20°C')
-line05 = plt.plot(tp[1], v_s600[4], 'r--*', lw=1.5)
-line06 = plt.plot(tp[1], v_s600[5], 'r--*', lw=1.5)
+line10 = plt.plot(tp[1], pv_np700[3], 'b--v', lw=1.5, label= '700 V @ T = -20°C')
+line11 = plt.plot(tp[1], pv_np700[4], 'b--v')
+line12= plt.plot(tp[1], pv_np700[5], 'b--v')
 
-line7 = plt.plot(tp[0], v_np700[0], 'b:*', lw=1.5, label= '700 V @ T = -26°C')
-line8 = plt.plot(tp[0], v_np700[1], 'b:*', lw=1.5)
-line9 = plt.plot(tp[0], v_np700[2], 'b:*', lw=1.5)
+line13 = plt.plot(tp[0], pv_np800[0], 'g--o', lw=1.5, label= '800 V @ T = -26°C')
+line14 = plt.plot(tp[0], pv_np800[1], 'g--o', lw=1.5)
+line15= plt.plot(tp[0], pv_np800[2], 'g--o', lw=1.5)
 
-line13 = plt.plot(tp[1], v_np700[3], 'b--v', lw=1.5, label= '700 V @ T = -20°C')
-line14 = plt.plot(tp[1], v_np700[4], 'b--v')
-line15= plt.plot(tp[1], v_np700[5], 'b--v')
-
-line10 = plt.plot(tp[0], v_np800[0], 'g--s', lw=1.5, label= '800 V @ T = -26°C')
-line11 = plt.plot(tp[0], v_np800[1], 'g--s', lw=1.5)
-line12= plt.plot(tp[0], v_np800[2], 'g--s', lw=1.5)
-
-line16 = plt.plot(tp[1], v_np800[3], 'g--o', lw=1.5, label= '800 V @ T = -20°C')
-line17 = plt.plot(tp[1], v_np800[4], 'g--o', lw=1.5)
-line18= plt.plot(tp[1], v_np800[5], 'g--o', lw=1.5)
+line16 = plt.plot(tp[1], pv_np800[3], 'g--v', lw=1.5, label= '800 V @ T = -20°C')
+line17 = plt.plot(tp[1], pv_np800[4], 'g--v', lw=1.5)
+line18= plt.plot(tp[1], pv_np800[5], 'g--v', lw=1.5)
 
 plt.legend()
 plt.grid(color = 'black', linestyle = '--', linewidth = 0.5)
-plt.axhline(y=261, xmin=0, xmax=1, color='k', linestyle='--', linewidth=2, label = '1% of 26112(261pixels)')
+plt.axhline(y=1, xmin=0, xmax=1, color='k', linestyle='--', linewidth=2)
+plt.show()
+
+# Noisy pixels Vs Temperature at constant Voltage:
+plt.figure(10)
+tp = [-26,-20]
+plt.ylabel('No. of Noisy Pixels',fontname="Times New Roman", fontweight="bold")
+plt.title('Noisy pixels vs Temperature[Only Stuck](595, bitten, $0.862$e16 $n_{eq} . cm^{-2}$)',fontname="Times New Roman", fontweight="bold")
+plt.xlabel('Temperature(℃)',fontname="Times New Roman", fontweight="bold")
+plt.axis([None, None, 0, 1.5])
+plt.yticks(np.arange(0,1.5,0.5))
+plt.rcParams["figure.figsize"] = [7.50,3.50]
+plt.rcParams["figure.autolayout"] = True
+#plt.xticks(ticks = tickvalues ,labels = labellist, rotation = 'vertical')
+
+line01 = plt.plot(tp[0], pv_s600[0], 'r--o', lw=1.5, label= '600 V @ T = -26°C')
+line02 = plt.plot(tp[0], pv_s600[1], 'r--o', lw=1.5)
+line03 = plt.plot(tp[0], pv_s600[2], 'r--o', lw=1.5)
+
+line04 = plt.plot(tp[1], pv_s600[3], 'r--v', lw=1.5, label= '600 V @ T = -20°C')
+line05 = plt.plot(tp[1], pv_s600[4], 'r--v', lw=1.5)
+line06 = plt.plot(tp[1], pv_s600[5], 'r--v', lw=1.5)
+
+line07 = plt.plot(tp[0], pv_s700[0], 'b--o', lw=1.5, label= '700 V @ T = -26°C')
+line08 = plt.plot(tp[0], pv_s700[1], 'b--o', lw=1.5)
+line09 = plt.plot(tp[0], pv_s700[2], 'b--o', lw=1.5)
+
+line010 = plt.plot(tp[1], pv_s700[3], 'b--v', lw=1.5, label= '700 V @ T = -20°C')
+line011 = plt.plot(tp[1], pv_s700[4], 'b--v')
+line012= plt.plot(tp[1], pv_s700[5], 'b--v')
+
+line013 = plt.plot(tp[0], pv_s800[0], 'g--o', lw=1.5, label= '800 V @ T = -26°C')
+line014 = plt.plot(tp[0], pv_s800[1], 'g--o', lw=1.5)
+line015= plt.plot(tp[0], pv_s800[2], 'g--o', lw=1.5)
+
+line016 = plt.plot(tp[1], pv_s800[3], 'g--v', lw=1.5, label= '800 V @ T = -20°C')
+line017 = plt.plot(tp[1], pv_s800[4], 'g--v', lw=1.5)
+line018= plt.plot(tp[1], pv_s800[5], 'g--v', lw=1.5)
+
+plt.legend()
+plt.grid(color = 'black', linestyle = '--', linewidth = 0.5)
+plt.axhline(y=1, xmin=0, xmax=1, color='k', linestyle='--', linewidth=2)
 plt.show()
 
 # =============================================================================
